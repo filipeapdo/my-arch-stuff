3af956ddb9cd918b347d643a29e55dd0c7bf4a3b
filipeapdo <filipeaparecido@gmail.com>
2024-08-17 08:52:52 -0300

    updated nvim ::: for neovim cross config

--- nvim/nvim/after/plugin/whichkey.lua
+++ nvim/nvim/after/plugin/whichkey.lua
@@ -4,20 +4,18 @@
 -- Document existing key chains
 require('which-key').add {
     -- [[ from lsp.lua ]]
-    ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-    ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-    -- ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-    -- ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-    ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-    ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-    ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    ['<leader>F'] = { name = ' LSP ::: [F]ormat', _ = 'which_key_ignore' },
+    { '<leader>c', group = '[C]ode' },
+    { '<leader>d', group = '[D]ocument' },
+    { '<leader>r', group = '[R]ename' },
+    { '<leader>f', group = '[F]ind' },
+    { '<leader>w', group = '[W]orkspace' },
+    { '<leader>F', group = ' LSP ::: [F]ormat' },
+    { '<leader>r', group = '[R]ename' },
 
     -- [[ from onedark.lua ]]
-    ['<leader>s'] = { name = '[S]tyle', _ = 'which_key_ignore' },
+    { '<leader>s', group = '[S]tyle' },
 
     -- [[ from remap.lua ]]
-    ['<leader>m'] = { name = '[M]arkdown', _ = 'which_key_ignore' },
-    ['<leader>p'] = { name = '[P]acker', _ = 'which_key_ignore' },
-    -- ['<leader>mt'] = { name = '[T]able', _ = 'which_key_ignore' },
+    { '<leader>m', group = '[M]arkdonw' },
+    { '<leader>p', group = '[P]acker' },
 }
